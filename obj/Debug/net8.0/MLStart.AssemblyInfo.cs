fde9a5f58591a4610c3993b5b605e0ca0e3f671a
Cacaoo <necacaoo@gmail.com>
2024-01-25 20:16:29 +0300

    Config manager service
    Added a class and methods for automatically creating a settings file. Added methods for adding values and changing them

--- obj/Debug/net8.0/MLStart.AssemblyInfo.cs
+++ obj/Debug/net8.0/MLStart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MLStart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9d608fc03a3c6bd25a41c58d73d30d9b4473e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MLStart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MLStart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
