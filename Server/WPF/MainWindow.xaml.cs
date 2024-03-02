b7e3c1e7b72ad1cdba80b93e2ed109732d0fede4
Cacaoo <necacaoo@gmail.com>
2024-03-02 17:54:59 +0300

    edit server start

--- Server/WPF/MainWindow.xaml.cs
+++ Server/WPF/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Server;
